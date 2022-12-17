313cb517c65250a74b357af7227617fae3f9d5d8
Lena-Kalmikov <100858741+Lena-Kalmikov@users.noreply.github.com>
2022-12-17 23:48:50 +0200

    ok

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
 import "./index.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+//import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Login from "./components/login";
 import Register from "./components/register";
+import { render } from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
+render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="Register" element={<Register />} />
             <Route path="TalkBack" element={<App />}></Route>
         </Routes>
-    </BrowserRouter>
+    </BrowserRouter>,
+    document.getElementById("root")
+    
 );
